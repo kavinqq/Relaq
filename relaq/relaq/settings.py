5a6413089a70aa72c656b8fd167a86ac7ff3f8dd
KuanLin <kuanlin72043@gmail.com>
2025-05-04 22:14:21 +0800

    feat: 加入uat docker設定

--- relaq/relaq/settings.py
+++ relaq/relaq/settings.py
@@ -50,7 +50,11 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://relaq_fe:3000",
 ]
+
+CORS_ALLOW_HEADERS = ["*"]
+CORS_ALLOW_CREDENTIALS = True
 
 
 MIDDLEWARE = [
